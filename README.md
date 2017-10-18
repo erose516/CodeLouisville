74eff7b58d37de1c9c835ffdf857fddd24621e36
Emily Zachery <emily.zachery@gmail.com>
2017-10-18 19:56:02 -0400

    Work completed during Code Louisville on 10/18/17

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a website for my business, Handy Miss. Right now, I have a live site that I created using Jimdo, but ultimately I'd like to replace it with a site I designed myself so that I can customize the site as I see fit.
+
+
+
+
+After completion, either:
+
+Open index.html
+
+Visit the GitHub Page here: 
---
+++ project.js
@@ -0,0 +1,12 @@
+/*
+
+Custom JS – can use framework such as AJAX, Angular, or jQuery, but can’t just import/link Bookstrap
+
+User interactivity
+-User input that is submitted (doesn’t have to go anywhere)
+-Click-through menu
+-Image carousel that responds to clicks
+-Lightbox pop-ups for images
+-Even just a single button with a pop-up that says says "This functionality not yet implemented"
+
+*/
